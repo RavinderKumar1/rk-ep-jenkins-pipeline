5a991b76970d5c12d122b23510e706a1d9f0b6cd
Ravinder Kumar <ravinderdhamija562@gmail.com>
2023-07-07 22:51:13 +0530

    Update service-develop-master.groovy

--- gke/service-develop-master.groovy
+++ gke/service-develop-master.groovy
@@ -1,47 +1,26 @@
-@Library('ns_pipeline_library')_
-def service_sha = null
-def dataplane_sha = null
-def dataplane_pkg_version = null
-def webui_sha = null
-def webui_branch = null
-def webui_url = null
-def dataplane_url = null
-def dataplane_branch = null
-def slack_channel = "#develop-builds"
-def drone_build_repos = [
-    "cci",
-    "irm",
-    "tokenservice",
-    "discoverygatekeeper",
-    "conductorservice"
-]
+@Library('ns_pipeline_library') _
 
-pipeline {
-    agent { label 'gcp-slave' }
-    environment {
-        BRANCH_TYPE = "develop"
-        NS_BUILD_DIR = "${env.WORKSPACE}"
-        VERSION = "1.develop-${RELEASE}.${BUILD_NUMBER}"
-        START_TIME = new Date().getTime();
-        GIT_REMOTE = "https://github.com/netSkope/service.git"
-    }
-    options {
-        timeout(time: params.MAX_DURATION ? params.MAX_DURATION : 12, unit: 'HOURS')
-        buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '30', numToKeepStr: '30');
-        timestamps()
-    }
-    stages {
-        stage('Get Repo map') {
-            steps {
-                script {
-                    ns_pipeline.get_repo_map()
-                }
-            }
+node {
+    stage('Test library function') {
+        /*
+            Description - Check default artifactory
+            Input       - None
+            E output    - artifactory
+        */
+        def default_artifactory
+        try {
+            default_artifactory = ns_pipeline.get_default_artifactory()
         }
-        stage('Run Gradle test') {
-            steps {
-                sh './gradlew test'
-            }
+        catch (e){
+            currentBuild.result = 'FAILURE'
+            default_artifactory = ''
         }
+        if ( default_artifactory == "artifactory") {
+            echo "Passed"
+        }
+        //def is_develop_job_env_input = ["develop_env1": "env1","develop_env2":"env2"]
+        //run_function("is_develop_job",is_develop_job_env_input,"","")
+        def repo_maps = ns_pipeline.get_repo_map()
+        echo "repo maps: ${repo_maps}"
     }
 }
