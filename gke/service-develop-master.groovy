d4efc479c6fb0927552ff4bb4b22078d1386cd5f
Ravinder Kumar <ravinderdhamija562@gmail.com>
2023-07-08 18:25:46 +0530

    Update service-develop-master.groovy

--- gke/service-develop-master.groovy
+++ gke/service-develop-master.groovy
@@ -24,7 +24,6 @@ node ('gcp-slave'){
         if ( default_artifactory == "artifactory") {
             echo "Passed"
         }
-        sleep 300
         //def is_develop_job_env_input = ["develop_env1": "env1","develop_env2":"env2"]
         //run_function("is_develop_job",is_develop_job_env_input,"","")
         def repo_maps = ns_pipeline.get_repo_map()
