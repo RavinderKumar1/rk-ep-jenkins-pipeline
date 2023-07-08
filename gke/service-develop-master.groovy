59befea50a470c9ca8c207fb55b2f5845aaf1529
Ravinder Kumar <ravinderdhamija562@gmail.com>
2023-07-08 18:40:19 +0530

    Update service-develop-master.groovy

--- gke/service-develop-master.groovy
+++ gke/service-develop-master.groovy
@@ -6,10 +6,10 @@ node ('gcp-slave'){
         git branch: 'main',
                 url: 'https://github.com/RavinderKumar1/rk-ep-jenkins-pipeline.git'
     }
-    
+
     stage('Test library function') {
         /*
-            Description - Check default artifactory
+            Description - Test type1 : simple, Check default artifactory
             Input       - None
             E output    - artifactory
         */
@@ -26,7 +26,15 @@ node ('gcp-slave'){
         }
         //def is_develop_job_env_input = ["develop_env1": "env1","develop_env2":"env2"]
         //run_function("is_develop_job",is_develop_job_env_input,"","")
-        def repo_maps = ns_pipeline.get_repo_map()
-        echo "repo maps: ${repo_maps}"
+        /*
+             Description - Test type2 : with env var, Override default artifactory
+             Input       - DEFAULT_ARTIFACTORY:OVERRIDDEN_ARTIFACTORY
+             E output    - OVERRIDDEN_ARTIFACTORY
+        */
+        env.setProperty('DEFAULT_ARTIFACTORY', 'OVERRIDDEN_ARTIFACTORY')
+        def _overridden_rtf = nsPipeline.get_default_artifactory()
+        echo "overridden artifactory: ${_overridden_rtf}"
+//        def repo_maps = ns_pipeline.get_repo_map()
+//        echo "repo maps: ${repo_maps}"
     }
 }
