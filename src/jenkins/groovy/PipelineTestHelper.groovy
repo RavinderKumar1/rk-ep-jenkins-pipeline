7890432872c74bcbd83434e3ef3dd9ebef1c6b6a
Ravinder Kumar <ravinderdhamija562@gmail.com>
2023-07-20 14:44:58 +0530

    Update PipelineTestHelper.groovy

--- src/jenkins/groovy/PipelineTestHelper.groovy
+++ src/jenkins/groovy/PipelineTestHelper.groovy
@@ -1,7 +1,14 @@
 package jenkins.groovy
 
+def obj
 class PipelineTestHelper {
     def helperFunction() {
         println('Calling helper function from PipelineTestHelper')
     }
+    PipelineTestHelper(obj) {
+        this.obj = obj
+        obj.println("Calling helper class constructor")
+        
+    }
+    
 }
