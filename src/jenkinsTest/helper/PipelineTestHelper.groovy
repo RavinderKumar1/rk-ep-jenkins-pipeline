186837e7aa9754ded4134b3fafd66373d4266959
Ravinder Kumar <ravinderk@netskope.com>
2023-07-20 15:19:08 +0530

    EP-25291: test1

--- src/jenkinsTest/helper/PipelineTestHelper.groovy
+++ src/jenkinsTest/helper/PipelineTestHelper.groovy
@@ -1,7 +1,12 @@
 package jenkinsTest.helper
 
+def obj
 class PipelineTestHelper {
     void helperFunction() {
         println('Calling helper function from PipelineTestHelper')
+    }
+    PipelineTestHelper(obj) {
+	this.obj = obj
+	obj.println("PipelineTest helper constructor called")
     }
 }
